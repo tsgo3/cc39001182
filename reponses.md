80617ea3ba1a4821dc7957abea973395c74962ca
tsgo3 <39001182@parisnanterre.fr>
2022-11-15 10:54:31 +0100

    Q2 affichage

--- reponses.md
+++ reponses.md
@@ -1,3 +1,8 @@
 Question 1
 
 Pour cette question, je vais utiliser le patron de l'abstract Factory puisque le but ici est de serialiser des chaînes de caractères, autrement dit on veut obtenir la même chose mais sous des format différents.
+
+
+Question 2 
+
+Je ne vois pas quel patron utiliser ici, l'interface étant implémenté par aucune classe, j'ai créé une classe l'implémentant de manière à récupérer les recettes via les 2 sites
--- src/main/java/cc39001182/q2/Cookingreference2.java
+++ src/main/java/cc39001182/q2/Cookingreference2.java
@@ -1,14 +1,15 @@
 package cc39001182.q2;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cookingreference2 implements CookingReferenceRetriever{
     @Override
     public Map<String, CookingReference> retrieveFromIngredient(String ingredient) {
         Marmitor m = new Marmitor();
-        System.out.println(m.rechercher(ingredient));
+        System.out.println(m.rechercher(ingredient).toString());
         GrossBouf gb = new GrossBouf();
-        System.out.println(gb.find(ingredient));
+        System.out.println(gb.find(ingredient).toString());
         return null;
     }
 }
